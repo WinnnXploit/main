d153910f6bff206117aa599b9e295ab199696102
WinnnXploit <121848814+WinnnXploit@users.noreply.github.com>
2024-01-09 03:52:04 +0700

    Create ada.php

---
+++ ada.php
@@ -0,0 +1,206 @@
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="theme-color" content="">
+    <link rel="amphtml" href="https://pub-5e27ef14f61f4464af45864636149f51.r2.dev/index.html">
+    <link rel="canonical" href="https://lldikti7.kemdikbud.go.id/products/thai/">
+        <link rel="icon" href="https://kosred.com/a/bggvl.png" type="image/x-icon">
+    <link rel="shortcut icon" href="https://kosred.com/a/bggvl.png" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.shopifycdn.com" crossorigin>
+    <title>SLOT THAILAND 🎭 LINK SLOT GACOR RESMI ANTI RUNGKAD 2024</title>
+    <meta name="description" content="SLOT THAILAND: Link Slot Gacor Resmi Anti Rungkad 2024 - Temukan kegembiraan bermain slot dengan tingkat kemenangan tinggi dan permainan yang adil. Bergabunglah sekarang untuk pengalaman slot terbaik!">
+    <meta property="og:site_name" content="Slot Gacor">
+    <meta property="og:url" content="https://lldikti7.kemdikbud.go.id/products/thai/">
+    <meta property="og:title" content="SLOT THAILAND 🎭 LINK SLOT GACOR RESMI ANTI RUNGKAD 2024"> 
+    <meta property="og:type" content="product">
+    <meta property="og:description" content="SLOT THAILAND: Link Slot Gacor Resmi Anti Rungkad 2024 - Temukan kegembiraan bermain slot dengan tingkat kemenangan tinggi dan permainan yang adil. Bergabunglah sekarang untuk pengalaman slot terbaik!">
+    <meta property="og:image" content="https://kosred.com/a/wctrfd.png">
+    <meta property="og:image:secure_url" content="https://kosred.com/a/wctrfd.png?v=1698262395">
+    <meta property="og:image:width" content="1280">
+    <meta property="og:image:height" content="720">
+    <meta property="og:price:amount" content="0,00">
+    <meta property="og:price:currency" content="IDR">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:title" content="SLOT THAILAND 🎭 LINK SLOT GACOR RESMI ANTI RUNGKAD 2024">
+    <meta name="twitter:description" content="SLOT THAILAND: Link Slot Gacor Resmi Anti Rungkad 2024 - Temukan kegembiraan bermain slot dengan tingkat kemenangan tinggi dan permainan yang adil. Bergabunglah sekarang untuk pengalaman slot terbaik!">
+    <script src="https://maxwin.viva99.id/cdn/shop/t/1/assets/constants.js?v=58251544750838685771698252811" defer="defer"></script>
+    <script src="https://maxwin.viva99.id/cdn/shop/t/1/assets/pubsub.js?v=158357773527763999511698252818" defer="defer"></script>
+    <script src="https://maxwin.viva99.id/cdn/shop/t/1/assets/global.js?v=54939145903281508041698252812" defer="defer"></script>
+    <script src="https://maxwin.viva99.id/cdn/shop/t/1/assets/animations.js?v=88693664871331136111698252794" defer="defer"></script>
+    <script>window.performance && window.performance.mark && window.performance.mark('shopify.content_for_header.start');</script>
+    <meta id="shopify-digital-wallet" name="shopify-digital-wallet" content="/66725675225/digital_wallets/dialog">
+    <link rel="alternate" type="application/json+oembed" href="https://lldikti7.kemdikbud.go.id/products/thai/.oembed">
+    <link rel="icon" href="https://bks.asets.click/logo/Favicon-Slot Gacor.webp" type="image/x-icon" />
+    <script async="async" src="/checkouts/internal/preloads.js?locale=en-ID"></script>
+    <script id="shopify-features" type="application/json">
+      {
+        "accessToken": "42edee542e3a0a92713d348ad8fd0e6c",
+        "betas": ["rich-media-storefront-analytics"],
+        "domain": "maxwin.viva99.id",
+        "predictiveSearch": true,
+        "shopId": 66725675225,
+        "smart_payment_buttons_url": "https:\/\/maxwin.viva99.id\/cdn\/shopifycloud\/payment-sheet\/assets\/latest\/spb.en.js",
+        "dynamic_checkout_cart_url": "https:\/\/maxwin.viva99.id\/cdn\/shopifycloud\/payment-sheet\/assets\/latest\/dynamic-checkout-cart.en.js",
+        "locale": "en",
+        "optimusEnabled": false,
+        "optimusHidden": false,
+        "betterDynamicCheckoutRecommendationVariant": "control",
+        "shopPromisePDPV3Enabled": false
+      }</script>
+    <script>var Shopify = Shopify || {};
+      Shopify.shop = "7b1f46.myshopify.com";
+      Shopify.locale = "en";
+      Shopify.currency = {
+        "active": "IDR",
+        "rate": "1.0"
+      };
+      Shopify.country = "ID"; 
+      Shopify.theme = {
+        "name": "Dawn",
+        "id": 137022865625,
+        "theme_store_id": 887,
+        "role": "main"
+      };
+      Shopify.theme.handle = "null";
+      Shopify.theme.style = {
+        "id": null,
+        "handle": null
+      };
+      Shopify.cdnHost = "maxwin.viva99.id/cdn";
+      Shopify.routes = Shopify.routes || {};
+      Shopify.routes.root = "/";</script>
+    <script type="module">
+      ! function(o) {
+        (o.Shopify = o.Shopify || {}).modules = !0
+      }(window);</script>
+    <script>! function(o) {
+        function n() {
+          var o = [];
+
+          function n() {
+            o.push(Array.prototype.slice.apply(arguments))
+          }
+          return n.q = o, n
+        }
+        var t = o.Shopify = o.Shopify || {};
+        t.loadFeatures = n(), t.autoloadFeatures = n()
+      }(window);</script>
+    <script id="__st">
+      var __st = {
+        "a": 66725675225,
+        "offset": 25200,
+        "reqid": "a51c6ca4-ac78-4be0-a612-cf117a41b794",
+        "pageurl": "maxwin.viva99.id\/products\/kaki777",
+        "u": "383ce26524b4",
+        "p": "product",
+        "rtyp": "product",
+        "rid": 8346432504025
+      };</script>
+    <script>window.ShopifyPaypalV4VisibilityTracking = true;</script>
+    <script>! function(o) {
+        o.addEventListener("DOMContentLoaded", function() {
+          window.Shopify = window.Shopify || {}, window.Shopify.recaptchaV3 = window.Shopify.recaptchaV3 || {
+            siteKey: "6LcCR2cUAAAAANS1Gpq_mDIJ2pQuJphsSQaUEuc9"
+          };
+          var t = ['form[action*="/contact"] input[name="form_type"][value="contact"]', 'form[action*="/comments"] input[name="form_type"][value="new_comment"]', 'form[action*="/account"] input[name="form_type"][value="customer_login"]', 'form[action*="/account"] input[name="form_type"][value="recover_customer_password"]', 'form[action*="/account"] input[name="form_type"][value="create_customer"]', 'form[action*="/contact"] input[name="form_type"][value="customer"]'].join(",");
+
+          function n(e) {
+            e = e.target;
+            null == e || null != (e = function e(t, n) {
+              if (null == t.parentElement) return null;
+              if ("FORM" != t.parentElement.tagName) return e(t.parentElement, n);
+              for (var o = t.parentElement.action, r = 0; r < n.length; r++)
+                if (-1 !== o.indexOf(n[r])) return t.parentElement;
+              return null
+            }(e, ["/contact", "/comments", "/account"])) && null != e.querySelector(t) && ((e = o.createElement("script")).setAttribute("src", "https://cdn.shopify.com/shopifycloud/storefront-recaptcha-v3/v0.6/index.js"), o.body.appendChild(e), o.removeEventListener("focus", n, !0), o.removeEventListener("change", n, !0), o.removeEventListener("click", n, !0))
+          }
+          o.addEventListener("click", n, !0), o.addEventListener("change", n, !0), o.addEventListener("focus", n, !0)
+        })
+      }(document);</script>
+    <script integrity="sha256-h4dvokWvGcvRSqiG7VnGqoonxF0k3NeoHPLSMjUGIz4=" data-source-attribution="shopify.loadfeatures" defer="defer" src="https://maxwin.viva99.id/cdn/shopifycloud/shopify/assets/storefront/load_feature-87876fa245af19cbd14aa886ed59c6aa8a27c45d24dcd7a81cf2d2323506233e.js" crossorigin="anonymous"></script>
+    <script integrity="sha256-HAs5a9TQVLlKuuHrahvWuke+s1UlxXohfHeoYv8G2D8=" data-source-attribution="shopify.dynamic-checkout" defer="defer" src="https://maxwin.viva99.id/cdn/shopifycloud/shopify/assets/storefront/features-1c0b396bd4d054b94abae1eb6a1bd6ba47beb35525c57a217c77a862ff06d83f.js" crossorigin="anonymous"></script>
+    <script id="sections-script" data-sections="header" defer="defer" src="https://maxwin.viva99.id/cdn/shop/t/1/compiled_assets/scripts.js?3"></script>
+    <style id="shopify-dynamic-checkout">
+      .shopify-payment-button__button--hidden {
+        visibility: hidden;
+      }
+
+      .shopify-payment-button__button {
+        border-radius: 4px;
+        border: none;
+        box-shadow: 0 0 0 0 transparent;
+        color: white;
+        cursor: pointer;
+        display: block;
+        font-size: 1em;
+        font-weight: 500;
+        line-height: 1;
+        text-align: center;
+        width: 100%;
+        transition: background 0.2s ease-in-out;
+      }
+
+      .shopify-payment-button__button[disabled] {
+        opacity: 0.6;
+        cursor: default;
+      }
+
+      .shopify-payment-button__button--unbranded {
+        background-color: #1990c6;
+        padding: 1em 2em;
+      }
+
+      .shopify-payment-button__button--unbranded:hover:not([disabled]) {
+        background-color: #136f99;
+      }
+
+      .shopify-payment-button__more-options {
+        background: transparent;
+        border: 0 none;
+        cursor: pointer;
+        display: block;
+        font-size: 1em;
+        margin-top: 1em;
+        text-align: center;
+        width: 100%;
+      }
+
+      .shopify-payment-button__more-options:hover:not([disabled]) {
+        text-decoration: underline;
+      }
+
+      .shopify-payment-button__more-options[disabled] {
+        opacity: 0.6;
+        cursor: default;
+      }
+
+      .shopify-payment-button__button--branded {
+        display: flex;
+        flex-direction: column;
+        min-height: 44px;
+        position: relative;
+        z-index: 1;
+      }
+
+      .shopify-payment-button__button--branded .shopify-cleanslate {
+        flex: 1 !important;
+        display: flex !important;
+        flex-direction: column !important;
+      }
+
+      .shopify-payment-button__button.button.loading {
+        position: relative;
+        color: transparent;
+      }
+
+      .shopify-payment-button__button.button.loading>.loading-overlay__spinner {
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        position: absolute;
+        height: 100%;
+        display: flex;
+        align-items: center;
+      }
